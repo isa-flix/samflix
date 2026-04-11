1def10651a39be9db7e56541621ab06945c5d35e
isa-flix <irfansaeedahmad@gmail.com>
2026-04-11 19:15:19 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,69 @@
+// SamFlix Movie Data
+
 const movies = [
-  {
-    title: "Movie 1",
-    image: "https://via.placeholder.com/300x450",
-    video: "https://www.w3schools.com/html/mov_bbb.mp4"
-  },
-  {
-    title: "Movie 2",
-    image: "https://via.placeholder.com/300x450",
-    video: "https://www.w3schools.com/html/movie.mp4"
-  }
+    {
+        id: "tt28650488",
+        title: "Mario Galaxy",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/aSmAmNAK2VYfb0rYlleYJ10s3CC.jpg"
+    },
+    {
+        id: "tt6718170",
+        title: "Super Mario Bros",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/uzNSqyEhB0Dsnmwjwslrez37jY5.jpg"
+    },
+    {
+        id: "tt0108255",
+        title: "Super Mario Bros 1993",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/cudfWiiFDxj8rq1Myolzv6yY3ah.jpg"
+    },
+    {
+        id: "tt26443616",
+        title: "Hoppers",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/xjtWQ2CL1mpmMNwuU5HeS4Iuwuu.jpg"
+    },
+    {
+        id: "tt1375666",
+        title: "Inception",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/xlaY2zyzMfkhk0HSC5VUwzoZPU1.jpg"
+    },
+    {
+        id: "tt0816692",
+        title: "Interstellar",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/yQvGrMoipbRoddT0ZR8tPoR7NfX.jpg"
+    },
+    {
+        id: "tt0468569",
+        title: "The Dark Knight",
+        img: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+    },
+    {
+        id: "tt0111161",
+        title: "Shawshank Redemption",
+        img: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
+    },
+    {
+        id: "tt1392170",
+        title: "The Hunger Games",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/dqGZnJiVwlanUab6R4SOP1E1qEs.jpg"
+    },
+    {
+        id: "tt1951264",
+        title: "Catching Fire",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/5EEeUbSPi3lr6YsLUpiGcYtRMWk.jpg"
+    },
+    {
+        id: "tt1951265",
+        title: "Mockingjay Part 1",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/gYfI9j00w4s4gsuIky9qELR56kH.jpg"
+    },
+    {
+        id: "tt1951266",
+        title: "Mockingjay Part 2",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/AdssSv6zeZQ8wCHHSM7InlXpExI.jpg"
+    },
+    {
+        id: "tt10545296",
+        title: "Ballad of Songbirds & Snakes",
+        img: "https://www.themoviedb.org/t/p/w600_and_h900_face/iYNnnkZshI5La1bJCrblUVA181F.jpg"
+    }
 ];
