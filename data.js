e2598eba06c396de4e23def428838785fd5bf0f0
isa-flix <irfansaeedahmad@gmail.com>
2026-04-11 19:13:37 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const movies = [
+  {
+    title: "Movie 1",
+    image: "https://via.placeholder.com/300x450",
+    video: "https://www.w3schools.com/html/mov_bbb.mp4"
+  },
+  {
+    title: "Movie 2",
+    image: "https://via.placeholder.com/300x450",
+    video: "https://www.w3schools.com/html/movie.mp4"
+  }
+];
