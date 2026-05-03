f3df847508a8ee73a3a7bf9c0fb657bf639783c4
isa-flix <irfansaeedahmad@gmail.com>
2026-05-03 04:13:57 -0400

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,1 +1,230 @@
+let movies = [
+/*
+  {
+    "id": "mp4_test",
+    "title": "MP4 Test Video",
+    "img": "live.png",
+    "category": "Movies test",
+    "video": "https://mega.nz/file/9DggDbYK#2rJ_dsH7PJMSKcfnSsuRt6SXEQCVVvVeJJYSEoeYET0"
+  },*/
+  {
+    "id": "001hls",
+    "title": "Willow Sports",
+    "img": "live.png",
+    "category": "Live Stream",
+    "video": "https://amg01269-amg01269c1-firetv-us-5377.playouts.now.amagi.tv/playlist.m3u8"
+  },
+	{
+    "id": "002hls",
+    "title": "BBC Earth",
+    "img": "live.png",
+    "category": "Live Stream",
+    "video": "https://amg00793-bbcstudios-amg00793c3-lg-us-2528.playouts.now.amagi.tv/playlist/amg00793-bbcstudios-bbcearta-lgus/playlist.m3u8"
+  },
+{
+    "id": "003hls",
+    "title": "ARY Digita",
+    "img": "live.png",
+    "category": "Live Stream",
+    "video": "https://arydigital.aryzap.com/dc2dc67658cc5a71a92476b702cd2ae8/69ea2a66/v1/0183ea2408f90b8ed5941a38bc72/0183ea24302d0b8ed5941a38bc75/634E5F8E000072F08E6D-p0_tmpl360p_360p-1776867496168.ts"
+  },
+/* ---------------- excempt later ---------------- */
+	
+	
+	{
+		"id": "4",
+		"title": "0",
+		"img": "live.png",
+		"category": "Junk Chunk"
+	},
+	
+/*
+	{
+    "id": "002hls",
+    "title": "Flick Of Furry",
+    "img": "live.png",
+    "category": "Live Stream",
+    "video": "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/58e55b14ad8e9c364d55f717/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c28ca66-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=671a0e89-5b21-406b-95a9-b395c4ef4b07"
+  },
+{
+    "id": "003hls",
+    "title": "Wu Tang Collection",
+    "img": "live.png",
+    "category": "Live Stream",
+    "video": "https://dbrb49pjoymg4.cloudfront.net/v1/master/3fec3e5cac39a52b2132f9c66c83dae043dc17d4/prod_default_xumo-ams-aws/master.m3u8?ads.xumo_channelId=99991745"
+  },
+*/
+
+	/* ---------------- Cartoons ---------------- */
+	
+	{
+		"id": "tt0121164",
+		"title": "Corpse Bride",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/isb2Qow76GpqYmsSyfdMfsYAjts.jpg",
+		"category": "Cartoons"
+	},
+	{
+		"id": "1007757",
+		"title": "Swapped",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/tHhxWxge06goXU6ZQH1hj7vK8Hd.jpg",
+		"category": "Cartoons"
+	},
+	{
+		"id": "tt27613895",
+		"title": "GOAT",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/wfuqMlaExcoYiUEvKfVpUTt1v4u.jpg",
+		"category": "Cartoons"
+	},
+	
+    { 
+		"id": "tt15678738",
+		"title": "David", 
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/7lFG1WrCwAxBfyGK8ahlBVzXno7.jpg",
+		"category": "Cartoons"
+	},
+	{ 
+		"id": "tt26443616", "title": "Hoppers", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/xjtWQ2CL1mpmMNwuU5HeS4Iuwuu.jpg", "category": "Cartoons" 
+	},
+/* ---------------- Movies ---------------- */
+	{
+		"id": "tt13570066",
+		"title": "Desert Warrior",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/sxn4XI0EPaF3IOagjRaQ3yxHHNS.jpg",
+		"category": "Movie"
+	},
+	{
+		"id": "tt33244668",
+		"title": "Anaconda 2025",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/qxMv3HwAB3XPuwNLMhVRg795Ktp.jpg",
+		"category": "Movie"
+	},
+	{
+		"id": "tt2120120",
+		"title": "Pixels 2015",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/4aE5mGxjP8S1YOE4lpwj6OQcLZy.jpg",
+		"category": "Movie"
+	},
+	{
+		"id": "tt3566834",
+		"title": "A Minecraft Movie",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/iPPTGh2OXuIv6d7cwuoPkw8govp.jpg",
+		"category": "Movie"
+	},
+	
+	{ "id": "tt2094766", "title": "Assassin's Creed", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/v0Grw2r5iHohH2TMepZnRQCpWB3.jpg", "category": "Movie" },
+	{ "id": "tt0383010", "title": "The Three Stooges 2012", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/gElz0Gu4Opl79upXWVA3FWPzPqn.jpg", "category": "Movie" },
+	{ "id": "tt15398776", "title": "Oppenheimer", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/2y48XTa483LRFIb5fDKOwr8DHWz.jpg", "category": "Movie" },
+	{ "id": "tt12042730", "title": "Project Hail Mary", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/wXmoUdNvLx8Iiplgbuc1svl3b2d.jpg", "category": "Movie" },
+
+	
+	/* ---------------- Hindi ---------------- */
+	{ "id": "tt29540862", "title": "Bhooth Bangla 2026", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/ArIS4vwUxdhm3j7tsTHmffdfU8W.jpg", "category": "Indian Movies" },
+
+
+	
+	/* ---------------- Movie Sets ---------------- */
+
+	/* ---------------- Cars ---------------- */
+
+	{"id": "tt0317219", "title": "Cars 2006", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/u4G8EkiIBZYx0wEg2xDlXZigTOZ.jpg", "category": "MovieSet", "set": "Cars"},
+	{"id": "tt1216475", "title": "Cars II 2011", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/vu2PuKtP24Php0vKzhEKbqft59Q.jpg", "category": "MovieSet", "set": "Cars"},
+	{"id": "tt3606752", "title": "Cars III 2017", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/jJ8TnHvWHaVadW5JJjGYsM07j9i.jpg", "category": "MovieSet", "set": "Cars"},
+
+		/* ---------------- Super Troopers ---------------- */
+
+	{"id": "tt0247745", "title": "Super Troopers 2001", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/xBvAHPOSSOdmjJ4Se5fT4ipYQy2.jpg", "category": "MovieSet", "set": "Super Troopers"},
+{"id": "tt0859635", "title": "Super Troopers 2018", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/57SZgdugVClIy22rfnANeBJ5KsN.jpg", "category": "MovieSet", "set": "Super Troopers"},
+	
+	/* ---------------- Toy Story ---------------- */
+
+	{"id": "tt0114709", "title": "Toy Story", "img": "https://www.themoviedb.org/t/p/w1280/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg", "category": "MovieSet", "set": "Toy story"},
+	{"id": "tt0120363", "title": "Toy Story 2", "img": "https://www.themoviedb.org/t/p/w1280/4rbcp3ng8n1MKHjpeqW0L7Fnpzz.jpg", "category": "MovieSet", "set": "Toy story"},
+	{"id": "tt0435761", "title": "Toy Story 3", "img": "https://www.themoviedb.org/t/p/w1280/AbbXspMOwdvwWZgVN0nabZq03Ec.jpg", "category": "MovieSet", "set": "Toy story"},
+	{"id": "tt1979376", "title": "Toy Story 4", "img": "https://www.themoviedb.org/t/p/w1280/w9kR8qbmQ01HwnvK4alvnQ2ca0L.jpg", "category": "MovieSet", "set": "Toy story"},
+	{"id": "tt29355505", "title": "Toy Story 5", "img": "https://www.themoviedb.org/t/p/w1280/gaiMtK2ll6BLdK1qKFuZGDwwEuD.jpg", "category": "MovieSet", "set": "Toy story"},
+
+
+
+/* ---------------- The Lego Movie ---------------- */
+
+	{"id": "tt1490017", "title": "The LEGO Movie", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/9klB7qKC9aCeGyyM4uU5hSA6xDV.jpg", "category": "MovieSet", "set": "The Lego Movie"},
+	{"id": "tt3513498", "title": "The LEGO Movie 2: The Second Part", "img": "https://www.themoviedb.org/t/p/w1280/ymHghL9iS7X0QpSDqp7jhiEDVFh.jpg", "category": "MovieSet", "set": "The Lego Movie"},
+	{"id": "tt4116284", "title": "The Lego Batman Movie", "img": "https://www.themoviedb.org/t/p/w1280/auwW8g1btlXdDCwHkhsD3XcQDxZ.jpg", "category": "MovieSet", "set": "The Lego Movie"},
+	{
+		"id": "tt9673190",
+		"title": "The Lego Batman Movie 2: The Second Part",
+		"img": "https://www.themoviedb.org/t/p/w1280/ymHghL9iS7X0QpSDqp7jhiEDVFh.jpg",
+		"category": "MovieSet",
+		"set": "The Lego Movie",
+		"disabled": true
+	},
+	{"id": "tt3014284", "title": "The Lego Ninjago Movie", "img": "https://www.themoviedb.org/t/p/w1280/etPGtOrDCuRAw4NI34RCOK6bAoS.jpg", "category": "MovieSet", "set": "The Lego Movie"},
+
+	/* ---------------- SOnic ---------------- */
+
+	
+	{ "id": "tt3794354", "title": "Sonic the Hedgehog", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/bljXY2zZSD6poD4dOPwIxQkFTUB.jpg", "category": "MovieSet", "set": "Sonic the Hedgehog" },
+	{ "id": "tt12412888", "title": "Sonic the Hedgehog 2", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/8E7mIpEpSATxX5JEuw55GYx9hfk.jpg", "category": "MovieSet", "set": "Sonic the Hedgehog" },
+	{ "id": "tt18259086", "title": "Sonic the Hedgehog 3", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/nyEr1VqvKx1YiesMC3oTB2fZvpY.jpg", "category": "MovieSet", "set": "Sonic the Hedgehog" },
+
+	/* ---------------- Zootopia ---------------- */
+
+    { "id": "tt2948356", "title": "Zootopia", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/9LRFNLthb0jfWC0HUDUWjYgpP9F.jpg", "category": "MovieSet", "set": "Zootopia"},
+    { "id": "1084242", "title": "Zootopia 2", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/bjUWGw0Ao0qVWxagN3VCwBJHVo6.jpg", "category": "MovieSet", "set": "Zootopia" },
+
+	/* ---------------- The Super Mario ---------------- */
+
+	{ "id": "tt28650488", "title": "Mario Galaxy", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/aSmAmNAK2VYfb0rYlleYJ10s3CC.jpg", "category": "MovieSet", "set": "Super Mario Bros" },
+	{ "id": "tt6718170", "title": "Super Mario Bros", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/uzNSqyEhB0Dsnmwjwslrez37jY5.jpg", "category": "MovieSet", "set": "Super Mario Bros" },
+	{ "id": "tt0108255", "title": "Super Mario Bros 1993", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/cudfWiiFDxj8rq1Myolzv6yY3ah.jpg", "category": "MovieSet", "set": "Super Mario Bros" },
+
+/* ---------------- Mortal Kombat ---------------- */
+
+	{"id": "tt0113855", "title": "Mortal Kombat 1995", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/jY4b61Jm8v5iayxYx0VRPaD0AF1.jpg", "category": "MovieSet", "set": "Mortal Kombat"},
+	{"id": "tt0119707", "title": "Mortal Kombat: Annihilation 1997", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/9PmtU0XrF2BwG186jWzMVJufZJQ.jpg", "category": "MovieSet", "set": "Mortal Kombat"},
+	{"id": "tt0293429", "title": "Mortal Kombat 2021", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/nkayOAUBUu4mMvyNf9iHSUiPjF1.jpg", "category": "MovieSet", "set": "Mortal Kombat"},
+	{"id": "tt17490712", "title": "Mortal Kombat II 2026 ", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/lIsMeDbwntNXSUVHmWMMRXEZOVc.jpg", "category": "MovieSet", "set": "Mortal Kombat"},
+//	{"id": "tt29355505", "title": "Mortal Kombat", "img": "https://www.themoviedb.org/t/p/w1280/gaiMtK2ll6BLdK1qKFuZGDwwEuD.jpg", "category": "MovieSet", "set": "Mortal Kombat"},
+
+	
+	/* ---------------- The Maze Runner ---------------- */
+	{
+		"id": "tt1790864",
+		"title": "The Maze Runner 2014",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/2rOonEo6b6wENhucsuoXXzoXejM.jpg",
+		"category": "MovieSet",
+		"set": "The Maze Runner",
+	},
+		{
+		"id": "tt4046784",
+		"title": "Maze Runner: The Scorch Trials 2015",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/pCIdUSdcSfXzSlBGjqfzIREeFIl.jpg",
+		"category": "MovieSet",
+		"set": "The Maze Runner",
+	},
+		{
+		"id": "tt4500922",
+		"title": "Maze Runner: The Death Cure 2018",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/2zYfzA3TBwrMC8tfFbpiTLODde0.jpg",
+		"category": "MovieSet",
+		"set": "The Maze Runner",
+	},
+
+/* ---------------- The Hunger Games ---------------- */
+
+	
+	{ "id": "tt1392170", "title": "The Hunger Games", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/3Z9LDLiNp9j3GthL02pzHt9d6r.jpg", "category": "MovieSet", "set": "The Hunger Games" },
+	{ "id": "tt1951264", "title": "The Hunger Games: Catching Fire", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/5EEeUbSPi3lr6YsLUpiGcYtRMWk.jpg", "category": "MovieSet", "set": "The Hunger Games" },
+	{ "id": "tt1951265", "title": "The Hunger Games: MokingJay Part I", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/gYfI9j00w4s4gsuIky9qELR56kH.jpg", "category": "MovieSet", "set": "The Hunger Games" },
+	{ "id": "tt1951266", "title": "The Hunger Games: MokingJay Part II", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/AdssSv6zeZQ8wCHHSM7InlXpExI.jpg", "category": "MovieSet", "set": "The Hunger Games" },
+	{ "id": "tt10545296", "title": "The Hunger Games: The Ballad of Songbirds & Snakes", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/iYNnnkZshI5La1bJCrblUVA181F.jpg", "category": "MovieSet", "set": "The Hunger Games" },
+	{ "id": "tt32558705", "title": "The Hunger Games: Sunrise on the Reaping", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/aGDKyv5825jQpbY55qk3r73PGts.jpg", "category": "MovieSet", "set": "The Hunger Games" },
+
+
+/* ---------------- Series ---------------- */
+/* ---------------- The Three Stooges ---------------- */
+/* ---------------- Season 01 1960 ---------------- */
+
+	
+];
 
