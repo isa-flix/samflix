594005e0a071c90744a1d656c81f9b2fe4219935
isa-flix <irfansaeedahmad@gmail.com>
2026-07-11 22:53:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let hlsInstance = null;
 /* ---------------- PROVIDERS ---------------- */
 
 const providers = [
-  { name: "wootly", tmdb: id => `https://wootly.ch/?=${id}` },
+  { name: "wootly", tmdb: id => `https://wootly.ch/?v=${id}` },
   { name: "Vidsrc RU", tmdb: id => `https://vidsrc-embed.ru/embed/movie?tmdb=${id}` },
   { name: "Vidsrc SU", tmdb: id => `https://vidsrc-embed.su/embed/movie?tmdb=${id}` },
   { name: "Vidsrc.me", tmdb: id => `https://vidsrc.me/embed/movie?tmdb=${id}` },
