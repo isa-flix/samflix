62890f73d4c58cc8931e23cd2fab6b4ad61ce21c
isa-flix <irfansaeedahmad@gmail.com>
2026-08-28 00:36:28 -0400

    Update movies.js

--- movies.js
+++ movies.js
@@ -66,6 +66,13 @@ Movie set: 235
 
 	/* ---------------- Cartoons ---------------- */
 
+	{ 
+		"id": "1315772",
+		"title": "Minions & Monsters (2026)",
+		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/4LwvU9SZc8QQzW1X1FAPhNbXnEU.jpg",
+		"category": "Cartoons"
+	},
+
 	{
 		"id": "1185806",
 		"title": "PAW Patrol: The Dino Movie (2026)",
@@ -127,13 +134,7 @@ Movie set: 235
 		"category": "Cartoons"
 	},
 
-	{ 
-		"id": "1315772",
-		"title": "Minions & Monsters (2026)",
-		"img": "https://www.themoviedb.org/t/p/w600_and_h900_face/4LwvU9SZc8QQzW1X1FAPhNbXnEU.jpg",
-		"category": "Cartoons"
-	},
-	
+		
 	{ 
 		"id": "tt26443616", "title": "Hoppers", "img": "https://www.themoviedb.org/t/p/w600_and_h900_face/xjtWQ2CL1mpmMNwuU5HeS4Iuwuu.jpg", "category": "Cartoons" 
 	},
